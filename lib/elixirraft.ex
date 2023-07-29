3058989ba4ed28a0099189e51cbb6ff3f3c970c3
Peter Nicholson <petergoods@hotmail.com>
2023-07-29 11:57:06 +0200

    Initial commit

---
+++ lib/elixirraft.ex
@@ -0,0 +1,18 @@
+defmodule Elixirraft do
+  @moduledoc """
+  Documentation for `Elixirraft`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elixirraft.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixirraft_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirraftTest do
+  use ExUnit.Case
+  doctest Elixirraft
+
+  test "greets the world" do
+    assert Elixirraft.hello() == :world
+  end
+end
